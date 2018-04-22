bf9eb7c2c0f77396aaa4ad41cb09c9cd3b09f0bc
Anshu Singh <anshu@Anshus-MacBook-Pro.local>
2018-04-22 11:07:14 +0530

    Routing issue resolved

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ConnectedRouter as Router } from 'react-router-redux'
+import { Switch, Route } from 'react-router-dom'
+
+import { Login } from './components/Login'
+import { Profile } from './components/Profile'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+
+import createHistory from 'history/createBrowserHistory'
+
+const history = createHistory()
+
+const Home = () => <div>Welcome Home Girl!</div>
+
+export const App = () => 
+<Provider store={store}>
+  <Router history={history}>
+    <Switch>
+      <Route path='/login' component={Login} />
+      <Route path='/profile' component={Profile} />
+      <Route path='/' component={Home} />
+    </Switch>
+  </Router>
+</Provider>  
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+})
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+    host: '0.0.0.0',
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [htmlPlugin]
+}
